f8774394e91dfbcb27fa12d207e759d108770c55
Emil <e.aminov@s7.ru>
2022-10-09 03:48:26 +0300

    :sparkles:

--- faust_app/app/preprocessor.py
+++ faust_app/app/preprocessor.py
@@ -1,4 +1,5 @@
 from app.logic.categorization import Categorizer
+from rutermextract import TermExtractor
 
 
 def preprocess_post(post):
@@ -6,4 +7,6 @@ def preprocess_post(post):
     if not kw:
         print(f"Not categorized tag: {post.tags}")
     post.tags = [post.tags, *kw]
+    term_extractor = TermExtractor()
+    post.keywords = [i for i in term_extractor(post.text)]
     return post
