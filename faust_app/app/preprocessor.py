05e8f7da06bcbe11349046246aed59e76fd41e37
Emil <e.aminov@s7.ru>
2022-10-09 04:13:23 +0300

    :sparkles:

--- faust_app/app/preprocessor.py
+++ faust_app/app/preprocessor.py
@@ -1,10 +1,25 @@
+import pymorphy2 as pymorphy2
 from app.logic.categorization import Categorizer
 from rutermextract import TermExtractor
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+def get_text_in_normal_form(text):
+    analyzer = pymorphy2.MorphAnalyzer()
+    normalized_text_list = []
+    tokens = word_tokenize(text)
+    for token in tokens:
+        normalized_text_list.append(analyzer.parse(token)[0].normal_form)
+    return normalized_text_list
+
 
 def filter_text(text):
-    # TODO: логика убирания лишних слов
-    return text
+    word_list = get_text_in_normal_form(text)
+    russian_stopwords = stopwords.words('russian')
+    filtered_words = [word for word in word_list if word not in russian_stopwords]
+    return ' '.join(filtered_words)
 
 
 def preprocess_post(post):
@@ -14,5 +29,5 @@ def preprocess_post(post):
     post.tags = [post.tags, *kw]
     term_extractor = TermExtractor()
     filtered_text = filter_text(post.text)
-    post.keywords = [i for i in term_extractor(post.text)]
+    post.keywords = [i for i in term_extractor(filtered_text)]
     return post
